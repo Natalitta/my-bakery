1491701d56b1645314b82c00285b6a7a64d108bf
Natalitta <>
2023-08-03 20:24:42 +0000

    Create models for bookings and create admin

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import MenuItem, BookItem
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(MenuItem)
+class MenuItemAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'type', 'vegan')
+    search_fields = ['title', 'content']
+    list_filter = ('type', 'vegan')
+    summernote_fields = ('content',)
--- booking/models.py
+++ booking/models.py
@@ -11,7 +11,7 @@ ITEM_TYPES = (
 
 class MenuItem(models.Model):
     # Model for menu items
-    name = models.CharField(max_length=30, unique=True)
+    title = models.CharField(max_length=30, unique=True)
     type = models.CharField(max_length=10, choices=ITEM_TYPES, default='cakes')
     description = models.TextField(default="")
     featured_image = CloudinaryField('image', default='placeholder')
@@ -20,21 +20,43 @@ class MenuItem(models.Model):
 
     class Meta:
         # Order by type and name
-        ordering = ['type', 'name']
+        ordering = ['type', 'title']
 
     def __str__(self):
-        return str(self.name)
+        return str(self.title)
 
 
 class Menu(models.Model):
     # Model to create a menu
-    name = models.CharField(max_length=25, unique=True)
+    title = models.CharField(max_length=25, unique=True)
     cakes = models.ManyToManyField('MenuItem', related_name='cakes')
     pastry = models.ManyToManyField('MenuItem', related_name='pastry')
 
     class Meta:
         # Order by name
-        ordering = ['name']
+        ordering = ['title']
 
     def __str__(self):
-        return str(self.name)
+        return str(self.title)
+
+# Choice fields
+BOOKING_TIME = ((1, "morning"), (2, "afternoon"), (3, "evening"))
+
+class BookItem(models.Model):
+
+    # Model for making bookings of menu items
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_customer")
+    booking_name = models.CharField(max_length=25)
+    booked_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE, related_name="ordered_item")
+    number_of_items = models.IntegerField(default=1)
+    booking_date = models.DateField()
+    delivery = models.BooleanField(default=False)
+    address = models.CharField(max_length=100)
+    booking_time = models.IntegerField(choices=BOOKING_TIME, default=1)
+
+    class Meta:
+        # Order by booking_date and then booking_time 
+        ordering = ['booking_date', 'booking_time']
+
+    def __str__(self):
+        return str(self.pk)
