0de28dad0c968339138d171db2a0bdf7284b0f48
Natalitta <>
2023-08-11 17:20:18 +0000

    Add views to place an order, update and delete

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView 
 from .models import MenuItem, Booking
 from .forms import BookingForm
+from datetime import timedelta, date
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
 
 
 class MenuList(generic.ListView):
@@ -10,17 +14,48 @@ class MenuList(generic.ListView):
     template_name = "index.html"
 
 
-class MenuDetail(View):
+class Order(LoginRequiredMixin, CreateView):
+    form_class = BookingForm
+    template_name = 'orders/booking.html'
+    success_url = "/orders/all_bookings/"
+    model = Booking
 
-    def get(self, request, *args, **kwargs):
-        queryset = MenuItem.objects.all()
-        menu_item = get_object_or_404(queryset,)
+    def form_valid(self, form):
+        return super(Order, self).form_valid(form)
 
-        return render(
-            request,
-            "booking/menu_detail.html",
-            {
-                "menu_item": menu_item,
-                "booking_form": BookingForm()
-            },
+
+class BookingsList(LoginRequiredMixin, ListView):
+    model = Booking
+    template_name = 'orders/all_bookings.html'
+
+    def get_queryset(self):
+        return Booking.objects.filter(
+                customer=self.request.user,
+                booking_date__gt=(date.today()-timedelta(days=1))
+            )
+
+
+class EditBooking(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    form_class = BookingForm
+    template_name = 'orders/edit_booking.html'
+    success_url = "/orders/all_bookings"
+    model = Booking
+
+    def form_valid(self, form):
+        messages.success(
+            self.request,
+            f'You have successfully updated your order.'
         )
+        return super(EditBooking, self).form_valid(form)
+
+
+class DeleteBooking(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Booking
+    success_url = "/booking/all_bookings"
+
+    def form_valid(self, form):
+        messages.success(
+            self.request,
+            'You have successfully deleted your order'
+        )
+        return super(DeleteBooking, self).form_valid(form)
